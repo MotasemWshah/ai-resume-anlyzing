5aaa8721fe02087f122820eca80257fcd445226d
MotasemWshah <motasemwshah37@gmail.com>
2025-09-16 19:38:40 +0300

    Authintaction

---
+++ app/routes/auth.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, type JSX } from "react";
+import { Link, useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+export const meta: () => Array<{ title?: string; name?: string; content?: string }> = () => [
+    { title: "Resumlyzer | Auth | " },
+    { name: "description", content: "log into your account" },
+];
+
+const Auth: () => JSX.Element = () => {
+    const { isLoading , auth } = usePuterStore();
+    const Location = useLocation();
+    const next = Location.search.split("next=")[1];
+    const navigate = useNavigate(); 
+    useEffect(() => {
+        if (auth.isAuthenticated) navigate(next);
+    }, [auth.isAuthenticated , next]);
+    return (
+<main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+<div className="gradint-border shadow-lg">
+    <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+        <div className="flex flex-col items-center gap-2 text-center">
+            <h1>Welcome</h1>
+            <h2>Log in for Making Your Job Dream Come True</h2>
+        </div>
+        <div>
+            {isLoading ? (
+                <button className="auth-button animate-pulse">
+                    <p>We Are Signing You in</p>
+                </button>
+            ) : (
+                <>
+                {auth.isAuthenticated ? (
+                    <button className="auth-button" onClick={auth.signOut}>
+                        <p>Log Out</p>
+                    </button>) : (
+                         <button className="auth-button" onClick={auth.signIn}>
+                        <p>Log In</p>
+                    </button>
+                    )}
+                </>
+            )}
+        </div>
+    </section>
+</div>
+</main>
+    )
+}
+export default Auth;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,43 @@
+import Navbar from "~/component/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import ResumeCard from "~/component/ResumeCard";
+import { resumes } from "../../constants";
+import { Link, useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
+import React from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumelyzing" },
+    { name: "description", content: "Get Your Honest ATS AI feedback" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+      const {auth } = usePuterStore();
+    const navigate = useNavigate(); 
+    useEffect(() => {
+        if (!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated]);
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+  <Navbar></Navbar>
+  <section className = "main-section">
+  <div className="page-heading py-16">
+<h1>Track Your Resume Application & Ratings</h1>
+<h2>Review Your Submissions and Check AI-Powered FeedBacks</h2>
+  </div>
+ 
+{resumes.length > 0 && (
+  <div className="resumes-section">
+     {resumes.map((resume) => (
+<ResumeCard key={resume.id} resume={resume}></ResumeCard>
+      ))}
+      </div>
+)}
+
+ </section>
+  
+ </main>
+
 }
